34c5172eea1d23d7fdead0805f1fd0535428e39b
codesweaver <codetrain999@gmail.com>
2021-05-11 23:41:49 +0900

    문자열 계산기. 요구사항 대로 만들어보기.

--- src/string/StringCalculator.java
+++ src/string/StringCalculator.java
@@ -7,28 +7,31 @@ public class StringCalculator {
 
     public int[] parseString(String input) {
 
-        String seperator = getSeperator(input);
+        Pattern pattern = Pattern.compile("//(.)\\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+
+        String seperator = "[,;]";
+        if (matcher.find()) {
+            seperator = matcher.group(1);
+            input = matcher.group(2);
+        }
+
+        return getIntegerArray(seperator, input);
+    }
+
+    public int[] getIntegerArray(String seperator, String input) {
+
         String[] inputs = input.split(seperator);
         int[] numbers = new int[inputs.length];
 
         for (int i=0; i<inputs.length; i++) {
             int number = Integer.parseInt(inputs[i]);
+            if (number < 0) throw new RuntimeException("음수를 입력할 수 없습니다");
             numbers[i] = number;
         }
         return numbers;
     }
 
-    public String getSeperator(String input) {
-        Pattern pattern = Pattern.compile("//(.)\\n(.*)");
-        Matcher matcher = pattern.matcher(input);
-
-        String result = "[,;]";
-        if (matcher.find()) {
-            result = matcher.group();
-            result = result.replace("//", "");
-        }
-        return result;
-    }
 
     public int add(String input) {
         int[] numbers = parseString(input);
--- test/string/StringCalculatorTest.java
+++ test/string/StringCalculatorTest.java
@@ -19,22 +19,31 @@ class StringCalculatorTest {
     @Test
     void rexExpr() {
         String s = "//;\n1;2;3";
+        String seperator = "[,;]";
+
         Pattern pattern = Pattern.compile("//(.)\\n(.*)");
         Matcher matcher = pattern.matcher(s);
 
-        String seperator = "[,;]";
         if (matcher.find()) {
-            seperator = matcher.group();
-            System.out.println(seperator);
-            // seperator = seperator.replace("//", "");
+            System.out.println(matcher.group(1));
+            System.out.println(matcher.group(2));
+
+            seperator = matcher.group(1);
+            s = matcher.group(2);
+            seperator = seperator.replace("//", "").trim();
         }
-        // System.out.println(seperator);
     }
 
     @Test
     void add() {
         StringCalculator cal = new StringCalculator();
         assertEquals(6, cal.add("1,2;3"));
+        assertEquals(6, cal.add("//@\n1@2@3"));
         assertEquals(6, cal.add("//;\n1;2;3"));
+        assertThrows(RuntimeException.class, ()-> {
+            // cal.add("-1");
+            // cal.add("1, 2, -1");
+            cal.add("//@\n1@2@-3");
+        });
     }
 }
